e93fd2fa75f040fe2ef0a628953a903a3b8e2e7d
Jessica Irakoze <jessicairakoze4@gmail.com>
2025-08-23 14:48:48 +0200

    Todo

--- src/components/ToDo.jsx
+++ src/components/ToDo.jsx
@@ -6,8 +6,14 @@ import { RiDeleteBin6Line } from "react-icons/ri";
 const ToDo = () => {
   const [input, setInput] = useState("");
   const [todos, setTodos] = useState([]);
+  const [count, setCount] = useState(0);
+
+  const handleDelete = (todoToRemove) => {
+    setTodos(todos.filter((todo) => todo !== todoToRemove));
+    setCount((prevCount) => prevCount - 1);
+  };
   return (
-    <div className="m-10 text-2xl border border-black rounded-2xl w-[50%] h-[50%] p-8">
+    <div className="m-10 text-xl border border-black rounded-2xl w-[30%] h-[50%] p-8">
       <div className="flex flex-row gap-4 justify-center">
         <IoMdCheckboxOutline size={28} />
         <h1 className="">Todo List</h1>
@@ -18,7 +24,7 @@ const ToDo = () => {
           type="text"
           name="todo"
           placeholder="Add a new task..."
-          className="border border-black rounded-2xl p-1 w-[100%] shadow-black"
+          className="border border-black rounded-2xl p-2 w-[90%] shadow-black"
           value={input}
           onChange={(e) => {
             setInput(e.target.value);
@@ -26,22 +32,55 @@ const ToDo = () => {
           }}
         />
         <CiSquarePlus
-          size={40}
-          onClick={() => setTodos((prevTodo) => [...prevTodo, input])}
+          size={43}
+          onClick={() => {
+            if (!input.trim()) return;
+            setTodos((prevTodo) => [
+              ...prevTodo,
+              { text: input, isChecked: false },
+            ]);
+            setInput("");
+          }}
         />
       </div>
       <ul>
         {todos.length > 0 &&
           todos.map((todo, index) => {
             return (
-              <div className="flex flex-row justify-between mt-4">
-                <input type="checkbox" name="todo" />
-                <li key={index}>{todo}</li>
-                <RiDeleteBin6Line size={28} />
+              <div
+                className="flex flex-row justify-between mt-4 items-center mb-6"
+                key={index}
+              >
+                <input
+                  type="checkbox"
+                  name="todo"
+                  className="bg-gray-500"
+                  checked={todo.isChecked}
+                  onChange={() =>
+                    setTodos((prevTodo) => {
+                      return prevTodo.map((todo, i) =>
+                        i == index
+                          ? { ...todo, isChecked: !todo.isChecked }
+                          : todo
+                      );
+                    })
+                  }
+                />
+                <li> {todo.isChecked ? <del>{todo.text}</del> : todo.text}</li>
+                <RiDeleteBin6Line
+                  className="bg-[#F2F4F5] text-red-500 rounded-full"
+                  size={23}
+                  onClick={() => handleDelete(todo)}
+                />
               </div>
             );
           })}
       </ul>
+      <hr />
+      <div className="flex justify-between">
+        <p> {count} remaining</p>
+        <p> {count} completed</p>
+      </div>
     </div>
   );
 };
